7dc42a7996ad43cf07b632f8c151c566a47f2482
T.Putra  Agastiya <ageztya.putra@gmail.com>
2024-01-26 16:32:14 +0700

    learn array

---
+++ tests/array.test.ts
@@ -0,0 +1,10 @@
+describe('Array',function(){
+    it('should same with JS',function(){
+
+        const names: string[] = ["qwerty","putra"]
+        const numbers: number[] = [1,2,3]
+
+        console.info(names)
+        console.info(numbers)
+    })
+})
